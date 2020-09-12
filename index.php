d7a890b83351ea36a0d902a5b550431ffe9f6e1d
AngeloMartinBHilado <56312511+AngeloMartinBHilado@users.noreply.github.com>
2020-09-12 17:26:23 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+
+	<head>
+	
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+	
+	</head>
+
+	<body>
+		<div id="bound2">
+			<div id="banner">
+				<div id="logoB">
+						<div id="HandleA"></div>
+						<div id="HandleB"></div>
+						<div id="logoC"></div>
+						<div id="Coffe"></div>
+					
+				</div>
+			
+				</div>
+        </div>	
+        </body>
+</html>
